de3c0b1ba8715ff7048c78d4442be654720a979f
Jaakko Haavisto <jaako.haavisto@kolumbus.fi>
2017-05-10 13:09:04 +0300

    ei kummosii muutoksii.

--- gonanvania_V0.03/Assets/CameraController.cs
+++ gonanvania_V0.03/Assets/CameraController.cs
@@ -3,68 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum CameraState { Normal, CutScene, Pause, Gameover };
+public enum CameraMode { Lerp, AverageSmooth, AllLocked };
 
 public class CameraController : MonoBehaviour {
 
     public CameraState currentState;
+    public CameraMode currentMode;
     public GameObject player;
     Vector3 playerPos;
-
-    //public float speedLookaheadFactor;
-
-    //Vector3 targetPosGizmo;
-
-    //public float smoothTime;
-    //Vector3 currentVelocity = Vector3.zero;
-    public float yOffset;
+    Vector3 targetPos;
+    Vector3 currentVelocity = Vector3.zero;
+    public float speedLookaheadFactor;
+    public float smoothTime;    
+    public float lerpOffset;
+    public float avgSmoothOffset;
     public float cameraZ;
     public float lerpFactor = 3;
     public float goDownAdd;
 
-    void Start () {
-        
-    }
-
-	void Update () {
-
-     
-	}
-
     void FixedUpdate() {
         var pos = transform.position;
-        var targetPos = pos;
+        targetPos = pos;
         playerPos = player.transform.position;
         targetPos = player.transform.position;
-        targetPos.x = playerPos.x;
         targetPos.z = cameraZ;
-        //if (player.GetComponent<Rigidbody>().velocity.y < 0) {
-        //    playerPos.y += -goDownAdd + yOffset;
-        //}
-        //else {
-        //    playerPos.y += yOffset;
-        //}
-        
-        //playerPos.z = cameraZ;
-        targetPos = Vector3.Lerp(pos, targetPos, Time.deltaTime * lerpFactor);
-        targetPos.y += yOffset;
-        targetPos.x = playerPos.x;
-        transform.position = targetPos;
-        //transform.position = new Vector3(playerPos.x, transform.position.y, cameraZ);
 
-
-        //targetPos.x = playerPos.x;
-        //targetPos.y = playerPos.y;
-
-        //targetPos.y += speedLookaheadFactor * player.GetComponent<Player>().smoothedVerticalSpeed + yOffset;
-        //targetPos.x = playerPos.x;
-
-        //transform.position = Vector3.SmoothDamp(pos, targetPos, ref currentVelocity, smoothTime);
-
-        //transform.position = new Vector3(playerPos.x, transform.position.y, cameraZ);
-        //targetPosGizmo = targetPos;
+        if (currentMode == CameraMode.Lerp) {         
+            
+            targetPos = Vector3.Lerp(pos, targetPos, Time.deltaTime * lerpFactor);
+            targetPos.y += lerpOffset;
+            targetPos.x = playerPos.x;
+            transform.position = targetPos;
+        } else if (currentMode == CameraMode.AverageSmooth) {
+            targetPos.y += speedLookaheadFactor * player.GetComponent<Player>().smoothedVerticalSpeed + avgSmoothOffset;
+            targetPos.x = playerPos.x;
+            targetPos.z = cameraZ;
+            transform.position = Vector3.SmoothDamp(pos, targetPos, ref currentVelocity, smoothTime);
+        } else if (currentMode == CameraMode.AllLocked) {
+            targetPos.y = playerPos.y + avgSmoothOffset;
+            transform.position = targetPos;
+        }
     }
 
     void OnDrawGizmos() {
-        //Gizmos.DrawWireSphere(targetPosGizmo, 1f);
+        Gizmos.DrawWireSphere(targetPos, 1f);
     }
 }
